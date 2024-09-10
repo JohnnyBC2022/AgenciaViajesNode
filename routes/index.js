ef239ac699f4dd0c39c9ecb12bed22eee351fd8d
Johnny <jonathanbc2012@gmail.com>
2024-09-10 13:54:17 +0200

    inicio view added

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
-    res.send('Inicio')
+    res.render('inicio')
 })
 router.get('/nosotros', (req, res) => {
-    res.render('nosotros')
+    const viajes = 'Viaje a Alemania'
+
+    res.render('nosotros', {
+        viajes
+    })
 })
 router.get('/contacto', (req, res) => {
     res.send('Contacto')
