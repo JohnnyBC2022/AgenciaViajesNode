2a9a032e45078143d04556cf57e59afbf2d7a64d
Johnny <jonathanbc2012@gmail.com>
2024-09-10 13:37:13 +0200

    routes added

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 // const express = require('express'); es la notación antigua de commonJS
 import express from 'express';
+import router from './routes/index.js';
 
 const app = express();
 
 // Definir el puerto
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
-    res.send('Inicio')
-})
-app.get('/nosotros', (req, res) => {
-    res.send('Nosotros')
-})
-app.get('/contacto', (req, res) => {
-    res.send('Contacto')
-})
+// Agregar router
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`El Servidor está funcionando en el puerto ${port}`)
